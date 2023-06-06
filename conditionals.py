b762cd772e83c1d13b5ecd875e06be86d7a0ab6c
Denys Romashkin <dr@adverty.com>
2023-06-06 16:32:01 +0300

    HW. conditionals

---
+++ conditionals.py
@@ -0,0 +1,24 @@
+# 1
+int_10 = 10
+int_20 = 20
+if int_10 > int_20:
+    print('Yes, this is True.')
+
+# 2
+x = 5
+y = 3
+if x > y:
+    print('x is bigger than y')
+elif y > x:
+    print('y is bigger than x')
+else:
+    print('x and y are equal')
+print('\n')
+
+# 3
+str_evil_genius = 'The world is mine.'
+str_mine = 'mine'
+if str_mine in str_evil_genius:
+    print('Yes, this is True.')
+else:
+    'No, it is not.'
